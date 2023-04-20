03b2f12bad1ef2011a039344cc14c75f336e7ff2
wirednkod <wirednkod@gmail.com>
2023-04-20 15:57:36 +0300

    Add some erroneously removed hook

---
+++ src/Context/useIsMountedRef.js
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react"
+
+export const useIsMountedRef = () => {
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  return isMounted
+}
